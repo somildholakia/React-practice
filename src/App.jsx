8c2924c440183afcc0b0d6a99031cbd4e8242709
somildholakia <somildholakia0852@gmail.com>
2026-02-27 19:22:38 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import Title from "./product";
 
 function App() {
 
+   let features = ["Nice height, good coding,logic solver"];
   return ( <>
-    <Title name="Somil" age="19" year="SYCS" /> 
+    <Title name="Somil" age="19" year="SYCS" features={features} /> 
     <Title name="ritik" age="99" year="FYCS"/>
     <Title name="krishna" age="39" year="TYCS"/>
 </>
--- src/product.jsx
+++ src/product.jsx
@@ -1,14 +1,14 @@
 import "./product.css";
 
-function Title({name,age,year }){
+function Title({name,age,year,features }){
+  // console.log(features)
   return (
     <div className="hello1">
       <h1>MY name is: {name}</h1>
       <p>My age is: {age} </p>
 
       <h2>My year is: {year}</h2>
-     
-
+     <p>{features}</p>
     </div>
   );
 }
